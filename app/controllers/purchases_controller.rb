6171d9f63c4c1be267dac9a98a3ba13250af81d4
Andy McCaskey <andy.mccaskey@gmail.com>
2014-09-25 14:12:34 -0400

    Added uuid to purchase object for more secure final URL

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,6 @@
 #purchases_controller.rb
 class PurchasesController < ApplicationController
   def show
-  	@purchase = Purchase.find(params[:id])
+  	@purchase = Purchase.find_by_uuid(params[:id])
   end
 end
