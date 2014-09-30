d7274e57bec52c643926638f11638d5da10e3dbd
Andy McCaskey <andy.mccaskey@gmail.com>
2014-09-30 16:58:58 -0400

    Added growth hacking course to seeds file for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+growth_hacking = Product.create(title: "Growth Hacking", 
+ subtitle: "Crash Course", author: "Mattan Griffel", 
+ length: "30 minutes", author_image_name: "teacher-image.jpg", 
+ price: "4.99", sku: "GROHACK1", download_url: "https://s3.amazonaws.com/one-month-stripe-payments/Peter+Bell+Resume.pdf", 
+ details: "You'll get one video", description: %{<p>A growth hacker is a rare combination: 
+ 	someone with the right marketing and technical skills who can come up with clever marketing
+ 	acks and also track their results.</p>
