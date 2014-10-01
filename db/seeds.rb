603fa5f929e34936e5384bda9c87aafaab6764ae
Andy McCaskey <andy.mccaskey@gmail.com>
2014-10-01 09:15:37 -0400

    Added growth hacking course to seeds file for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 growth_hacking = Product.create(title: "Growth Hacking", 
- subtitle: "Crash Course", author: "Mattan Griffel", 
+ subtitle: "Crash Course", author: "Mattan Griffel", price: "4.99", sku:"GROHACK1",
  length: "30 minutes", author_image_name: "teacher-image.jpg", 
  price: "4.99", sku: "GROHACK1", download_url: "https://s3.amazonaws.com/one-month-stripe-payments/Peter+Bell+Resume.pdf", 
- details: "You'll get one video", description: %{<p>A growth hacker is a rare combination: 
- 	someone with the right marketing and technical skills who can come up with clever marketing
- 	acks and also track their results.</p>
+ details: "You'll get one video", description: %{ <p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
+
+            <p>In this talk, Mattan Griffel introduces you to the concept of Growth Hacking and shares his favorite tips for getting started as a growth hacker.</p>
+            <p><strong>What You'll Learn</strong></p>
+            <ul class="no-indent">
+                <li>What is a growth hacker?</li>
+                <li>The 5 stages of the user lifecycle</li>
+                <li>How to apply the Lean Marketing Framework</li>
+                <li>Resources and tools you'll need to know</li>
+            </ul>
+}
